78ba33df75040dc8802ab63aa374b47c02a5e469
YosukeSuzuki <panchhopper@gmail.com>
2021-04-19 18:18:13 +0900

    Zissen

---
+++ Zissen/work1/Main.java
@@ -0,0 +1,9 @@
+// public class Main {
+//   public static void main(String[] args) {
+//     final String FORMAT = "%-8s  %-8s  所持金%,5d";
+//     String s = String.format(FORMAT, "tom", "hero", 30000);
+//     String s1 = String.format(FORMAT, "sugawara", "herohero", 30000000);
+//     System.out.println(s);
+//     System.out.println(s1);
+//   }
+// }
