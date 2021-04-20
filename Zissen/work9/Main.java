4c09cd1623c094851571395ec437c6706e6f5ec9
YosukeSuzuki <panchhopper@gmail.com>
2021-04-20 18:01:45 +0900

    Zissen

--- Zissen/work9/Main.java
+++ Zissen/work9/Main.java
@@ -1,38 +1,21 @@
 import java.io.*;
-import java.util.zip.GZIPOutputStream;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String inFile = args[0];
     String outFile = args[1];
     
-    FileInputStream fis = null;
-    GZIPOutputStream gzos = null;
+    FileInputStream fis = new FileInputStream(inFile);
+    FileOutputStream fos = new FileOutputStream(outFile, true);
     
-    try {
-      fis = new FileInputStream(inFile);
-      FileOutputStream fos = new FileOutputStream(outFile);
-      BufferedOutputStream bos = new BufferedOutputStream(fos);
-      gzos = new GZIPOutputStream(bos);
-      
-      int i = fis.read();
-      while (i != -1) {
-        gzos.write(i);
-        i = fis.read();
-      }
-      gzos.flush();
-      fis.close();
-      gzos.close();
-    } catch (IOException e) {
-      System.out.println("ファイル操作に失敗しました");
-      try {
-        if (fis != null) {
-          fis.close();
-        }
-        if (gzos != null) {
-          gzos.close();
-        }
-      } catch (IOException ee) { }
+    int i = fis.read();
+    while(i != -1) {
+      fos.write(i);
+      i = fis.read();
     }
+    
+    fos.flush();
+    fos.close();
+    fis.close();
   }
 }
